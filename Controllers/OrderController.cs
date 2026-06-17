ce586d87e1d798f42e14d0245a1b710ba5998ef5
Fatmakkose <akkosefatma17@gmail.com>
2026-06-17 15:12:29 +0300

    Siparişler

--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -40,6 +40,7 @@ namespace UretimPlanlama.Controllers
             ViewBag.Workshops = _context.Workshops.OrderBy(w => w.Name).ToList();
             ViewBag.Fabricators = _context.Fabricators.OrderBy(f => f.Name).ToList();
             ViewBag.Customers = _context.Customers.OrderBy(c => c.Name).ToList();
+            ViewBag.Colors = _context.ColorDefs.OrderBy(c => c.Name).ToList();
             return View();
         }
 
